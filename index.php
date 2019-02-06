77407311d0374d64555c062d59e60f9585988b51
antmdev <executivesales@hotmail.co.uk>
2019-02-06 22:03:58 +0000

    initial commit - basic layout and styling completed

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <title>Welcome to Foodle</title>
+</head>
+
+<body>
+
+<Div class="wrapper indexPage">
+    <div class="mainSection">
+        <div class="logoContainer">
+            
+        <img src="/doodle/assets/images/festisite_google.png" alt="google logo">
+        
+        </div> <!-- logoContainer -->
+
+            <div class="searchContainer">
+
+                <form action="search.php" method="GET">
+                
+                <input class="searchBox" type="text" name="term">
+                <input class="searchButton" type="submit" name="Search" >
+                
+                </form>
+
+
+
+        </div> <!-- searchContainer -->
+    </div> <!-- mainSection -->
+</div>   <!-- indexPage -->
+</body>
+</html>
---
+++ search.php
@@ -0,0 +1,68 @@
+<?php
+    if(isset ($_GET["term"])) {
+        $term = $_GET["term"];
+    } else { 
+        exit("You must enter a search term"); //ends all code after this point and displsays this message
+    }
+    $type = isset ($_GET["type"]) ? $_GET["type"] : "Sites" //shorthand version
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <title>Welcome to Doodle</title>
+</head>
+
+<body>
+
+<div class="wrapper">   
+    <div class="header">
+        <div class="headerContent">
+
+            <div class="logoContainer">
+                <a href="index.php">
+                    <img src="/doodle/assets/images/festisite_google.png" alt="google logo">
+                </a>
+                </div><!-- logoContainer --> 
+
+                <div class="searchContainer">
+
+                    <form action="search.php" method="GET">
+
+                        <div class="searchBarContainer">
+                        <input type="text" class="searchBox" name="term">
+                        <button class="searchButton">
+                        <img src="assets/images/icons/icons8-search-24.png">
+                        </button>
+                    
+                    </form><!-- search Form -->
+                </div><!-- searchBarContainer --> 
+
+                
+                </div><!-- searchContainer --> 
+                </div> <!-- HeaderContent --> 
+            
+                <div class="tabsContainer">
+                    <ul class="tabList">
+                    <!-- Set class active if search type is sites or images or none  --> 
+                        <li class = "<?php echo $type == 'sites' ? 'active' : '' ?> "> 
+                            <a href="<?php echo 'search.php?term=$term&type=sites'; ?> ">
+                            Sites
+                            </a>
+                        </li>
+                        <li class = "<?php echo $type == 'images' ? 'active' : '' ?> ">
+                            <a href="<?php echo 'search.php?term=$term&type=images'; ?> "> 
+                            Images
+                            </a>
+                        </li>
+                    </ul>
+
+            </div><!-- tabsContainer -->
+        </div> <!-- header -->  
+    </div> <!-- wrapper -->
+</body>
+</html>
